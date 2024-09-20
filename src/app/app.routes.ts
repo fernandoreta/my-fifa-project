5af1055643d15c26ac64398d3e141cb05eb257c4
fernando.reta <freta@finmancorp.com>
2024-09-20 18:35:35 -0500

    components generation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TeamListComponent } from './team-list/team-list.component';
+import { TopScoresComponent } from './top-scores/top-scores.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'team-list',
+        component: TeamListComponent
+    },
+    {
+        path: 'top-scores',
+        component: TopScoresComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    }
+];
---
+++ src/app/top-scores/top-scores.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TopScoresComponent } from './top-scores.component';
+
+describe('TopScoresComponent', () => {
+  let component: TopScoresComponent;
+  let fixture: ComponentFixture<TopScoresComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TopScoresComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TopScoresComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
